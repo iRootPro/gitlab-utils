9a4b73469a78cb3dc581c89b20d8e369a51ee9f0
Alexander Neupokoev <admin@armavir.ru>
2024-10-03 17:00:26 +0300

    gitlab variables

---
+++ cmd/gitlab-variables/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Config представляет структуру для хранения переменных
+type Config struct {
+	Variables map[string]string `yaml:"variables"`
+}
+
+func extractVariables(filename string) (map[string]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config.Variables, nil
+}
+
+func main() {
+	allVariables := make(map[string]string)
+
+	// Список файлов для обработки
+	files := []string{".gitlab-ci.yml", ".gitlab-ci-usr.yml"}
+
+	for _, file := range files {
+		if _, err := os.Stat(file); err == nil {
+			fmt.Printf("Extracting from %s\n", file)
+			variables, err := extractVariables(file)
+			if err != nil {
+				fmt.Printf("Error reading %s: %v\n", file, err)
+				continue
+			}
+			// Слияние переменных
+			for k, v := range variables {
+				allVariables[k] = v
+			}
+		}
+	}
+
+	// Запись в файл variables.env
+	envFile, err := os.Create("variables.env")
+	if err != nil {
+		fmt.Printf("Error creating variables.env: %v\n", err)
+		return
+	}
+	defer envFile.Close()
+
+	for key, value := range allVariables {
+		fmt.Fprintf(envFile, "%s=\"%s\"\n", key, value)
+	}
+
+	fmt.Println("Generated variables.env file with merged variables")
+}
